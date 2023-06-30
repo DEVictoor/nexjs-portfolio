b3f9509b2d5356982f895fcfd07c42c136ebc937
Victor Mireles Bernabe <devictormireles@gmail.com>
2023-06-29 20:52:28 -0500

    add: bio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home() {
           justifyContent={"center"}
         >
           <Text fontSize={["5xl", "6xl"]} textAlign={"center"} as="b">
-            I&apos;m Victor Mireles
+            I&apos;m Victor Mireles Me gusta jugar lol,ect
           </Text>
           <Text textAlign={"center"} fontSize={["3xl", "4xl"]}>
             Back-end developer
