d4b3d1f861cfbd6bdd9ffb80f8cb000d194ab9e2
Victor Mireles Bernabe <devictormireles@gmail.com>
2023-07-26 16:20:39 -0500

    fix: link about

--- src/data/links.tsx
+++ src/data/links.tsx
@@ -3,7 +3,7 @@ export const Enlaces: ITab[] = [
   { link: "/projects", title: "Projects", id: 2 },
   { link: "/work", title: "Work", id: 3 },
   // { link: "/blog", title: "Blogs", id: 3 },
-  { link: "/about", title: "About", id: 4 },
+  // { link: "/about", title: "About", id: 4 },
 ];
 
 export interface ITab {
